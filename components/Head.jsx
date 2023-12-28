0820d5550b5709bb0af3460006f490b01ac24bc6
paras11917 <paras11917@gmail.com>
2023-12-28 23:57:05 +0530

    first commit

--- components/Head.jsx
+++ components/Head.jsx
@@ -17,8 +17,11 @@ const CustomHead = ({ title }) => {
         property="og:description"
         content="A full-stack developer building websites that you'd like to use."
       />
-      <meta property="og:image" content="https://imgur.com/4zi5KkQ.png" />
-      <meta property="og:url" content="https://vscode-portfolio.vercel.app" />
+      <meta
+        property="og:image"
+        content="https://drive.google.com/file/d/1Vmss6iTT6g0tu3NdA9DhddSg863-sNmA/preview"
+      />
+      <meta property="og:url" content="https://paraspipre.vercel.app" />
       <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
